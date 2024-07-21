32d2eea368dad2950d6b428a68ce086b0160aa51
Kseylo <alekseysarychev29@gmail.com>
2024-07-21 12:57:27 +0700

    Router initialized

--- packages/client/src/app/App.tsx
+++ packages/client/src/app/App.tsx
@@ -1,14 +1,47 @@
 import './App.css'
-import { createTheme, MantineProvider, Button } from '@mantine/core'
+import { createTheme, MantineProvider } from '@mantine/core'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from '../pages/home'
+import SignIn from '../pages/sign-in'
+import SignUp from '../pages/sign-up'
+import Profile from '../pages/profile'
+import Forum from '../pages/forum'
+import Leaderboard from '../pages/leaderboard'
+import { Routes } from '../shared/config/routes'
 
 const theme = createTheme({})
+
+const router = createBrowserRouter([
+  {
+    index: true,
+    element: <Home />,
+  },
+  {
+    path: Routes.SIGN_IN,
+    element: <SignIn />,
+  },
+  {
+    path: Routes.SIGN_UP,
+    element: <SignUp />,
+  },
+  {
+    path: Routes.PROFILE,
+    element: <Profile />,
+  },
+  {
+    path: Routes.FORUM,
+    element: <Forum />,
+  },
+  {
+    path: Routes.LEADERBOARD,
+    element: <Leaderboard />,
+  },
+])
 
 function App() {
   return (
     <MantineProvider theme={theme} defaultColorScheme={'dark'}>
-      <main>
-        <Button>Тестовая кнопка</Button>
-      </main>
+      <RouterProvider router={router} />
     </MantineProvider>
   )
 }
---
+++ packages/client/src/shared/config/routes/index.ts
@@ -0,0 +1,7 @@
+export enum Routes {
+  SIGN_IN = '/sign-in',
+  SIGN_UP = '/sign-up',
+  PROFILE = '/profile',
+  FORUM = '/forum',
+  LEADERBOARD = '/leaderboard',
+}
