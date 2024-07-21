00ce899c059272852eea471517b5bc3fb63e5e0b
Kseylo <alekseysarychev29@gmail.com>
2024-07-21 14:25:02 +0700

    SignIn Page base form ui

--- packages/client/src/pages/sign-in/index.tsx
+++ packages/client/src/pages/sign-in/index.tsx
@@ -1,3 +1,41 @@
+import {
+  Anchor,
+  Button,
+  Container,
+  Divider,
+  Group,
+  Paper,
+  PasswordInput,
+  Stack,
+  Text,
+  TextInput,
+} from '@mantine/core'
+import { Link } from 'react-router-dom'
+import { Routes } from '../../shared/config/routes'
+
 export default function SignIn() {
-  return <div>Sign In Page</div>
+  return (
+    <Container size={'xs'}>
+      <Paper radius={'md'} p={'xl'} withBorder>
+        <Text size={'lg'}>Добро пожаловать</Text>
+        <Divider my={'lg'} />
+        <form>
+          <Stack>
+            <TextInput label={'Логин'} placeholder={'Введите логин'} required />
+            <PasswordInput
+              label={'Пароль'}
+              placeholder={'Введите пароль'}
+              required
+            />
+          </Stack>
+          <Group justify={'space-between'} mt={'xl'}>
+            <Anchor component={Link} to={Routes.SIGN_UP}>
+              Нет аккаунта? Зарегистрируйтесь
+            </Anchor>
+            <Button type={'submit'}>Войти</Button>
+          </Group>
+        </form>
+      </Paper>
+    </Container>
+  )
 }
