5dec616e45ae52e863053f3af0c43225d4e1c833
Kseylo <alekseysarychev29@gmail.com>
2024-07-29 20:25:24 +0700

    Added Appshell, navlinks

--- packages/client/src/app/ui/protected-layout/index.tsx
+++ packages/client/src/app/ui/protected-layout/index.tsx
@@ -1,5 +1,53 @@
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet, useLocation } from 'react-router-dom'
+import { useDisclosure } from '@mantine/hooks'
+import { AppShell, Group, Burger, Title, NavLink } from '@mantine/core'
+import { navLinks } from '@/shared/config/routes'
 
 export default function ProtectedLayout() {
-  return <Outlet />
+  const [mobileOpened, { toggle: toggleMobile }] = useDisclosure()
+  const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true)
+  const location = useLocation()
+
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+        collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+      }}
+      padding={'md'}>
+      <AppShell.Header>
+        <Group h={'100%'} px={'md'}>
+          <Burger
+            opened={mobileOpened}
+            onClick={toggleMobile}
+            hiddenFrom="sm"
+            size="sm"
+          />
+          <Burger
+            opened={desktopOpened}
+            onClick={toggleDesktop}
+            visibleFrom="sm"
+            size="sm"
+          />
+          <Title>Гвинт</Title>
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p={'md'}>
+        {navLinks.map(link => (
+          <NavLink
+            key={link.label}
+            component={Link}
+            to={link.href}
+            label={link.label}
+            active={location.pathname === link.href}
+          />
+        ))}
+      </AppShell.Navbar>
+      <AppShell.Main>
+        <Outlet />
+      </AppShell.Main>
+    </AppShell>
+  )
 }
