e088a6590a8921efa35a7239696cedf4a15658ea
Kseylo <alekseysarychev29@gmail.com>
2024-08-02 16:04:57 +0700

    Mini-landing home page

--- packages/client/src/shared/config/routes/index.ts
+++ packages/client/src/shared/config/routes/index.ts
@@ -2,6 +2,7 @@ export enum Routes {
   HOME = '/',
   SIGN_IN = '/sign-in',
   SIGN_UP = '/sign-up',
+  GAME = '/game',
   PROFILE = '/profile',
   THREADS = '/threads',
   LEADER_BOARD = '/leader-board',
@@ -9,6 +10,7 @@ export enum Routes {
 
 export const navLinks = [
   { label: 'Главная', href: Routes.HOME },
+  { label: 'Играть', href: Routes.GAME },
   { label: 'Профиль', href: Routes.PROFILE },
   { label: 'Треды', href: Routes.THREADS },
   { label: 'Лидерборд', href: Routes.LEADER_BOARD },
