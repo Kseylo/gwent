4f0a83f65782ae38ef5ee007a1f9cd3fd8429aeb
Kseylo <alekseysarychev29@gmail.com>
2024-08-02 16:08:44 +0700

    Fixed test

--- packages/client/src/app/App.test.tsx
+++ packages/client/src/app/App.test.tsx
@@ -1,28 +1,28 @@
 import App from './App'
 import { render, screen } from '@testing-library/react'
-import { Provider } from 'react-redux';
-import fetch from 'cross-fetch';
+import { Provider } from 'react-redux'
+import fetch from 'cross-fetch'
 
-const appContent = 'Home Page'
+const appContent = 'Карточная игра во вселенной Ведьмака'
 
 const initialState = {
   UserReducer: {
     user: {
-      "id": 123,
-      "first_name": "Petya",
-      "second_name": "Pupkin",
-      "display_name": "Petya Pupkin",
-      "phone": "+79001001100",
-      "login": "userLogin",
-      "avatar": "/path/to/avatar.jpg",
-      "email": "string@ya.ru"
-    }
-  }
-};
+      id: 123,
+      first_name: 'Petya',
+      second_name: 'Pupkin',
+      display_name: 'Petya Pupkin',
+      phone: '+79001001100',
+      login: 'userLogin',
+      avatar: '/path/to/avatar.jpg',
+      email: 'string@ya.ru',
+    },
+  },
+}
 
 import configureStore from 'redux-mock-store'
-import {thunk} from 'redux-thunk'
-import {AppDispatch} from "@/store";
+import { thunk } from 'redux-thunk'
+import { AppDispatch } from '@/store'
 
 const middlewares: any[] = [thunk] // add your middlewares like `redux-thunk`
 const mockStore = configureStore(middlewares)
@@ -30,15 +30,15 @@ const mockStore = configureStore(middlewares)
 // You would import the action from your codebase in a real scenario
 function success() {
   return {
-    type: 'FETCH_DATA_SUCCESS'
+    type: 'FETCH_DATA_SUCCESS',
   }
 }
 
-function getUser () {
-  return (dispatch: AppDispatch ) => {
+function getUser() {
+  return (dispatch: AppDispatch) => {
     return fetch('https://jsonplaceholder.typicode.com/users/1') // Some async action with promise
       .then(() => dispatch(success()))
-  };
+  }
 }
 
 it('should execute fetch data', () => {
@@ -46,14 +46,13 @@ it('should execute fetch data', () => {
 
   // Return the promise
   // @ts-ignore
-  return store.dispatch(getUser())
-    .then(() => {
-      const actions = store.getActions()
-      expect(actions[0]).toEqual(success())
-    })
+  return store.dispatch(getUser()).then(() => {
+    const actions = store.getActions()
+    expect(actions[0]).toEqual(success())
+  })
 })
 
-test('Example test',  () => {
+test('Example test', () => {
   const store = mockStore(initialState)
 
   render(
