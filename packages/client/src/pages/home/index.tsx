05359fd9ea6aa3590d757abaeb6beed6a5527ba1
Alexey Lozhkin <78409487+Kseylo@users.noreply.github.com>
2024-08-02 16:12:59 +0700

    Mini-landing home page (#13)
    * Mini-landing home page
    
    * Fixed test

--- packages/client/src/pages/home/index.tsx
+++ packages/client/src/pages/home/index.tsx
@@ -1,3 +1,24 @@
+import { Container, Image, Title, Text, Button, Center } from '@mantine/core'
+import { Link } from 'react-router-dom'
+import { Routes } from '@/shared/config/routes'
+
 export default function Home() {
-  return <div>Home Page</div>
+  return (
+    <Container>
+      <Image src={'/hero.avif'} radius={'sm'} alt={'hero'} />
+      <Title my={'md'} ta={'center'}>
+        Карточная игра во вселенной Ведьмака
+      </Title>
+      <Text size={'xl'} ta={'center'}>
+        Гвинт — это карточная мини-игра. Цель игры — набрать больше очков, чем
+        противник, используя различные карты. Игроки поочередно выкладывают
+        карты, формируя свою боевую линию, пока не будет определен победитель.
+      </Text>
+      <Center mt={'xl'}>
+        <Button size={'xl'} component={Link} to={Routes.GAME}>
+          Начать игру
+        </Button>
+      </Center>
+    </Container>
+  )
 }
